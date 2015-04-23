8f8914fc702302d00d39e9c93f60dbeb7f4e2483
rythos42 <Blyrble7>
2015-04-23 14:37:35 -0700

    Updating manifest

--- release.bat
+++ release.bat
@@ -6,8 +6,8 @@ xcopy /e /y www .\platforms\android\assets\www
 
 call .\platforms\android\cordova\build --release
 "%JAVA_HOME%\bin\jarsigner" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore keystore .\platforms\android\bin\Malifaux-release-unsigned.apk "malifaux models"
-zipalign -v -f 4 .\platforms\android\bin\Malifaux-release-unsigned.apk MalifauxModels.apk
+"%ANDROID_HOME%\build-tools\android-4.4W\zipalign" -v -f 4 .\platforms\android\bin\Malifaux-release-unsigned.apk MalifauxModels.apk
 
 echo .
 echo Try the short pw.
-echo Remember to change platforms\Android\AndroidManifest.xml
+echo !!! Remember to change platforms\Android\AndroidManifest.xml
